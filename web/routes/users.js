22a05e426d9544ec077bbd6cd94092e3da4e6f41
xykera <akrai9554@gmail.com>
2024-11-27 14:15:54 +0530

    Update users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -6,4 +6,8 @@ router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
 
+router.get('/', function(req, res, next){
+  res.send('hello')
+})
+
 module.exports = router
